c7a982fd45676880d4acd51143769a80ff2e429b
Abigail Gillespie <ag00137@my.westga.edu>
2024-11-19 10:51:06 -0500

    I added a textbox to the form to diaplay the seven letters, a textbox so the user can enter words, and a button to submit the words. Then I added code in the MainForm class that creates a list and puts letters in and seven random letters are chosen. Then I added code to display them in the textbox.

--- AbbieGillespieProject2/AbbieGillespieProject2/MainForm.Designer.cs
+++ AbbieGillespieProject2/AbbieGillespieProject2/MainForm.Designer.cs
@@ -28,18 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            letterLbl1 = new Label();
+            inputTxtBox = new TextBox();
+            submitBtn = new Button();
+            wordLbl = new Label();
+            lettersLbl = new Label();
+            lettersTxtBox = new TextBox();
             SuspendLayout();
             // 
-            // Form1
+            // letterLbl1
+            // 
+            letterLbl1.AutoSize = true;
+            letterLbl1.Location = new Point(216, 97);
+            letterLbl1.Name = "letterLbl1";
+            letterLbl1.Size = new Size(0, 25);
+            letterLbl1.TabIndex = 0;
+            // 
+            // inputTxtBox
+            // 
+            inputTxtBox.Location = new Point(289, 228);
+            inputTxtBox.Name = "inputTxtBox";
+            inputTxtBox.Size = new Size(237, 31);
+            inputTxtBox.TabIndex = 1;
+            // 
+            // submitBtn
+            // 
+            submitBtn.Location = new Point(289, 312);
+            submitBtn.Name = "submitBtn";
+            submitBtn.Size = new Size(112, 34);
+            submitBtn.TabIndex = 2;
+            submitBtn.Text = "Submit";
+            submitBtn.UseVisualStyleBackColor = true;
+            submitBtn.Click += submitBtn_Click;
+            // 
+            // wordLbl
+            // 
+            wordLbl.AutoSize = true;
+            wordLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Underline);
+            wordLbl.Location = new Point(158, 234);
+            wordLbl.Name = "wordLbl";
+            wordLbl.Size = new Size(125, 25);
+            wordLbl.TabIndex = 3;
+            wordLbl.Text = "Enter a word:";
+            // 
+            // lettersLbl
+            // 
+            lettersLbl.AutoSize = true;
+            lettersLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Underline);
+            lettersLbl.Location = new Point(208, 188);
+            lettersLbl.Name = "lettersLbl";
+            lettersLbl.Size = new Size(75, 25);
+            lettersLbl.TabIndex = 4;
+            lettersLbl.Text = "Letters:";
+            // 
+            // lettersTxtBox
+            // 
+            lettersTxtBox.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            lettersTxtBox.Location = new Point(289, 172);
+            lettersTxtBox.Name = "lettersTxtBox";
+            lettersTxtBox.ReadOnly = true;
+            lettersTxtBox.Size = new Size(237, 47);
+            lettersTxtBox.TabIndex = 5;
+            lettersTxtBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(983, 592);
+            Controls.Add(lettersTxtBox);
+            Controls.Add(lettersLbl);
+            Controls.Add(wordLbl);
+            Controls.Add(submitBtn);
+            Controls.Add(inputTxtBox);
+            Controls.Add(letterLbl1);
+            Name = "MainForm";
             Text = "Text Twist by Gillespie";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label letterLbl1;
+        private TextBox inputTxtBox;
+        private Button submitBtn;
+        private Label wordLbl;
+        private Label lettersLbl;
+        private TextBox lettersTxtBox;
     }
 }
--- AbbieGillespieProject2/AbbieGillespieProject2/MainForm.cs
+++ AbbieGillespieProject2/AbbieGillespieProject2/MainForm.cs
@@ -5,6 +5,61 @@ namespace AbbieGillespieProject2
         public MainForm()
         {
             InitializeComponent();
+            GetLetters();
+        }
+
+        private List<char> letterList = new List<char>();
+        private Random random = new Random();
+        private char[] currLetters = new char[7];
+
+        private void GetLetters()
+        {
+            letterList.Clear();
+            letterList.AddRange(new string('e', 11));
+            letterList.AddRange(new string('t', 9));
+            letterList.AddRange(new string('o', 8));
+            letterList.AddRange(new string('a', 6));
+            letterList.AddRange(new string('i', 6));
+            letterList.AddRange(new string('n', 6));
+            letterList.AddRange(new string('s', 6));
+            letterList.AddRange(new string('h', 5));
+            letterList.AddRange(new string('r', 5));
+            letterList.AddRange(new string('l', 4));
+            letterList.AddRange(new string('d', 3));
+            letterList.AddRange(new string('u', 3));
+            letterList.AddRange(new string('w', 3));
+            letterList.AddRange(new string('y', 3));
+            letterList.AddRange(new string('b', 2));
+            letterList.AddRange(new string('c', 2));
+            letterList.AddRange(new string('f', 2));
+            letterList.AddRange(new string('g', 2));
+            letterList.AddRange(new string('m', 2));
+            letterList.AddRange(new string('p', 2));
+            letterList.AddRange(new string('v', 2));
+            letterList.Add('j');
+            letterList.Add('k');
+            letterList.Add('q');
+            letterList.Add('x');
+            letterList.Add('z');
+
+            for (int i = 0; i < 7; i++)
+            {
+                int index = random.Next(letterList.Count);
+                currLetters[i] = letterList[index];
+                letterList.RemoveAt(index);
+            }
+
+            DisplayLetters();
+        }
+
+        private void DisplayLetters()
+        {
+            lettersTxtBox.Text = string.Join(" ", currLetters);
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
